1de146c62fe8679e1ba8ff5090489f97055b5e29
Adi Widyawan <adi.widyawan@bocistudio.com>
2019-07-03 14:47:32 +0700

    Add print dropdown menu on resident edit page.

--- app/views/residents/edit.blade.php
+++ app/views/residents/edit.blade.php
@@ -13,6 +13,20 @@
   <div class="col-lg-12 col-md-12">
     <ul class="nav nav-tabs">
       @include("residents/tabs", ['tab' => $tab])
+      <li class="dropdown pull-right">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          <i class="glyphicon glyphicon-print"></i>
+          Print
+          <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Biodata</a></li>
+          <li><a href="#">ID Card Application Form (F-1.21)</a></li>
+          <li><a href="#">Domicile</a></li>
+          <li><a href="#">Police Records Application (SKCK)</a></li>
+          <li><a href="#">Poor Annotation (SKTM)</a></li>
+        </ul>
+      </li>
     </ul>
     <div class="tab-content">
       <div class="tab-pane active">
